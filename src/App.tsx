aba1196d84aff79b4fe71201f23be519e41a7adf
trekswithnick <nicholasbrowningdev@outlook.com>
2025-02-01 09:58:08 +1000

    able to connect to NestJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
 import router from "./components/Routes";
+import { ApolloProvider } from "@apollo/client";
+import client from "./constants/apollo-client";
+import React from "react";
 
 const darkTheme = createTheme({
   palette: {
@@ -11,13 +14,17 @@ const darkTheme = createTheme({
 function App() {
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline>
-        <Container>
-          <RouterProvider router={router} />
-        </Container>
-      </CssBaseline>
-    </ThemeProvider>
+    <React.StrictMode>
+      <ApolloProvider client={client}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline>
+          <Container>
+            <RouterProvider router={router} />
+          </Container>
+        </CssBaseline>
+      </ThemeProvider>
+    </ApolloProvider>
+    </React.StrictMode>
   )
 }
 
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 import { Link as MUILink } from "@mui/material";
 import Auth from "./Auth";
+import { useCreateUser } from "../../hooks/useCreateUser";
 
 const SignUp = () => {
+    const [createUser] = useCreateUser();
+
     return (
         <>
-            <Auth submitLabel="Sign Up" onSubmit={async () => {}}>
+            <Auth submitLabel="Sign Up" onSubmit={async ({ email, password }) => {
+                await createUser({
+                    variables: {
+                        createUserInput: {
+                            email,
+                            password
+                        }
+                    }
+                })
+            }}>
                 <Link to={'/login'} style={{ alignSelf: 'center' }}>
                     <MUILink>Log In</MUILink>
                 </Link>
